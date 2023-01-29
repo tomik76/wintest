e9b7f40dd9aa8df0b2a425b99843e778b16a2adb
tomik76 <98682579+tomik76@users.noreply.github.com>
2023-01-29 21:34:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,26 +2,18 @@
 
 using namespace std;
 #include <windows.h>
-int main()
-{
-    cout<<"Hello World";
 
-
-
-
-
-
-  
-
+void TypeKeyboard(char* text) {
+    for (int i = 0; text[i]; i++) {
+        keybd_event(VkKeyScan(text[i]), 0, 0, 0);
+        keybd_event(VkKeyScan(text[i]), 0, KEYEVENTF_KEYUP, 0);
+    }
 }
+
+int main() {
+    char text[] = "Hello, World!";
+    TypeKeyboard(text);
 
     return 0;
 }
 
-void TypeKeyboard(char* text) {
-  for (int i = 0; i < strlen(text); i++) {
-    keybd_event(VkKeyScan(text[i]), 0, 0, 0);
-    keybd_event(VkKeyScan(text[i]), 0, KEYEVENTF_KEYUP, 0);
-  }
-    
-TypeKeyboard("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmcddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd") 

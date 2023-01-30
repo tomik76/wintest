8349e8d3b0e926e21ce2a9f1f5d5de7255ab5136
tomik76 <98682579+tomik76@users.noreply.github.com>
2023-01-30 07:57:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-
+#include <iostream>
+#include <chrono>
+#include <thread>
 #include <windows.h>
 typedef unsigned char byte;
 byte my_byte = 0x01;
 void TypeKeyboard(char* text) {
     for (int i = 0; text[i]; i++) {
         keybd_event(VkKeyScan(text[i]), 0, 0, 0);
+        std::this_thread::sleep_for(std::chrono::seconds(5));
         keybd_event(VkKeyScan(text[i]), 0, KEYEVENTF_KEYUP, 0);
     }
 }

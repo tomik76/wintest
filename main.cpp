57f6638d75c380738124131dd44e7a8cd58e0a13
tomik76 <98682579+tomik76@users.noreply.github.com>
2023-01-30 14:09:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main() {
         TypeKeyboard("###Long comment");
         keybd_event(VK_RETURN, 0, 0, 0);
         keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
-        TypeKeyboard("This issue can be caused by missing runtime dependencies. On Windows, compiled programs typically require the Visual C++ runtime to be installed on the target system.You can try to package the Visual C++ runtime with your program, but this increases the size of your installation package. Another option is to distribute your program as a standalone executable, which includes the runtime libraries in a single file. To create a standalone executable, you can use tools such as the statically-linked version of the Visual C++ runtime.");
+  #      TypeKeyboard("This issue can be caused by missing runtime dependencies. On Windows, compiled programs typically require the Visual C++ runtime to be installed on the target system.You can try to package the Visual C++ runtime with your program, but this increases the size of your installation package. Another option is to distribute your program as a standalone executable, which includes the runtime libraries in a single file. To create a standalone executable, you can use tools such as the statically-linked version of the Visual C++ runtime.");
           keybd_event(VK_CONTROL, 0, 0, 0);
           keybd_event(0x53, 0, 0, 0); // 0x53 is the virtual-key code for 'S'
     keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);

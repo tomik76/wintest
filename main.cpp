94da8f5da967cbaee79e1d23b8e72311f6b17453
tomik76 <98682579+tomik76@users.noreply.github.com>
2023-01-30 14:23:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@ void TypeKeyboard(char* text) {
 
 int main() {
     
-    for (int i = 0; i < 10000; i++) {        
-        TypeKeyboard("###Long comment");
+    for (int i = 0; i < 10000; i++) { 
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        TypeKeyboard("Long comment");
         keybd_event(VK_RETURN, 0, 0, 0);
         keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
-  #      TypeKeyboard("This issue can be caused by missing runtime dependencies. On Windows, compiled programs typically require the Visual C++ runtime to be installed on the target system.You can try to package the Visual C++ runtime with your program, but this increases the size of your installation package. Another option is to distribute your program as a standalone executable, which includes the runtime libraries in a single file. To create a standalone executable, you can use tools such as the statically-linked version of the Visual C++ runtime.");
-          keybd_event(VK_CONTROL, 0, 0, 0);
+         keybd_event(VK_CONTROL, 0, 0, 0);
           keybd_event(0x53, 0, 0, 0); // 0x53 is the virtual-key code for 'S'
     keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
     keybd_event(0x53, 0, KEYEVENTF_KEYUP, 0);

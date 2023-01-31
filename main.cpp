5859ae5945dd673c797f2ba4a24dabb2da8dad85
tomik76 <98682579+tomik76@users.noreply.github.com>
2023-01-31 16:49:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
     
     for (int i = 0; i < 10000; i++) { 
         std::this_thread::sleep_for(std::chrono::seconds(5));
-        TypeKeyboard("Long comment The error  is typically encountered when using the Cl.exe compiler to build a C++ project, and indicates that the linker is unable to find the C++ Standard Library (libstdc++.dll) that is required to build your project.");
+        TypeKeyboard("Long comment The error  is typically encountered when using the Cl.exe compiler to build a C++ project, and dgfgdgdgddgdgindicates that the linker is unable to find the C++ Standard Library (libstdc++.dll) that is required to build your project.");
         keybd_event(VK_RETURN, 0, 0, 0);
         keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
         TypeKeyboard("You can try to package the Visual C++ runtime with your program, but this increases the size of your installation package. Another option is to distribute your program as a standalone executable, which includes the runtime libraries in a single file. To create a standalone executable, you can use tools such as the statically-linked version of the Visual C++ runtime.");
